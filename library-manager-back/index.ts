6555c2ae31dc6b9a4c966d602d8c81c71d714fbb
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-06 21:14:05 +0100

    Update for development purposes

--- library-manager-back/index.ts
+++ library-manager-back/index.ts
@@ -1,14 +1,17 @@
 import express, { json } from "express";
 import 'express-async-errors'
 import cors from 'cors';
+import { userRouter } from "./routers/user";
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
 
 app.use(json());
+
+app.use('/user', userRouter)
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
