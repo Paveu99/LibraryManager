91c591ecc5e4150fd1cb0c8fbc97e80f4d6d76f0
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-06 18:20:47 +0100

    Types created

---
+++ library-manager-back/types/rentals/rental.entity.ts
@@ -0,0 +1,10 @@
+export type Rental = {
+    id: number,
+    user_id: string,
+    book_id: string,
+    rental_date: string,
+    return_date: string,
+    status: "rented" | "returned" | "overdue"
+}
+
+export type RentalDto = Omit<Rental, "id">
