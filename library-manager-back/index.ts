431c27d9a04917611cdd3f81a2ae0cdb08c9ad1b
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-09 22:57:03 +0100

    All of the routers used in the app

--- library-manager-back/index.ts
+++ library-manager-back/index.ts
@@ -2,6 +2,8 @@ import express, { json } from "express";
 import 'express-async-errors'
 import cors from 'cors';
 import { userRouter } from "./routers/user";
+import { bookRouter } from "./routers/books";
+import { rentalRouter } from "./routers/rentals";
 
 const app = express()
 
@@ -11,7 +13,9 @@ const app = express()
 
 app.use(json());
 
-app.use('/user', userRouter)
+app.use('/user', userRouter);
+app.use('/book', bookRouter);
+app.use('/rental', rentalRouter);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
