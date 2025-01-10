0e401de382367fe0390345724ccb80bb26d0c069
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-10 22:34:41 +0100

    Test file

---
+++ library-manager-front/src/Test.tsx
@@ -0,0 +1,23 @@
+import { useGetBooksQuery } from "./queries/books/useGetBooksQuery";
+
+export const Test = () => {
+
+    // const { data, error, isLoading } = useGetBooksQuery();
+
+    // console.log(data);
+    // console.log(error);
+
+
+    // if (isLoading) return <p>Loading</p>
+    // if (error) return <p>Error: {error.message}</p>
+
+
+    // return <div>
+    //     <ul>
+    //         {data?.map((el) => (
+    //             <li key={el.id}>{el.title}</li>
+    //         ))}
+    //     </ul>
+    // </div>
+    return null
+}
