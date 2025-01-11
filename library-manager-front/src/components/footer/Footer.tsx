f4820cd427c6f672caf12476d2d2c1a263a055fb
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-11 21:40:37 +0100

    Footer component created

---
+++ library-manager-front/src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>Courtesy of Pawel Jarecki</p>
+            <p>All rights reserved to the author</p>
+        </footer>
+    );
+};
