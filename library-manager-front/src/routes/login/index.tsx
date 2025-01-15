305197195809d182575ba789ea41f60ffcd8378c
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-15 07:50:14 +0100

    LoginForm and RegisterForm are being passed to the router

--- library-manager-front/src/routes/login/index.tsx
+++ library-manager-front/src/routes/login/index.tsx
@@ -1,9 +1,31 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { LoginForm } from '../../components/forms';
+import { useUserStore } from '../../store/useUserStore';
+import { useShallow } from 'zustand/shallow';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/login/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/login/"!</div>
+  const { role } = useUserStore(
+    useShallow((state) => ({
+      role: state.role,
+    }))
+  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (role !== 'guest') {
+      navigate({
+        to: '/',
+      });
+    }
+  }, [role, navigate]);
+
+  if (role === 'guest') {
+    return <LoginForm />;
+  }
+  return null;
 }
