3f89ba673c0cf825471a90a27aa4bc5f98d7a5ad
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-15 07:50:37 +0100

    Mutation functions created

---
+++ library-manager-front/src/queries/users/useLogoutUserMutation.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useApi } from "../../hooks/useApi";
+import { useUserStore } from "../../store/useUserStore";
+import { useShallow } from "zustand/shallow";
+
+export const useLogoutMutation = () => {
+    const { id } = useUserStore(
+        useShallow((state) => ({
+            id: state.id,
+        }))
+    );
+    const { apiPost } = useApi();
+    const queryClient = useQueryClient();
+
+    const { mutate, data, error, isPending, isSuccess } = useMutation({
+        mutationKey: ["userreg"],
+        mutationFn: async () => {
+            return apiPost<null, null>("user/logout", null, id);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["userreg"],
+            });
+        },
+    });
+
+    return {
+        data,
+        mutate,
+        isPending,
+        error,
+        isSuccess,
+    };
+};
+
