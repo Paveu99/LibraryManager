689bd58ae99d33558e47065a03db77f5847b5949
Paweł Jarecki <paweljarecki10@gmail.com>
2025-01-23 20:24:53 +0100

    Headers adjustments

--- library-manager-front/src/components/header/index.ts
+++ library-manager-front/src/components/header/index.ts
@@ -1,1 +1,2 @@
 export * from './mainHeader'
+export * from './userHeader'
---
+++ library-manager-front/src/components/header/userHeader/UserHeader.tsx
@@ -0,0 +1,15 @@
+import { Link, useLocation } from "@tanstack/react-router";
+import styles from './styles.module.scss';
+
+export const UserHeader = () => {
+    const { pathname } = useLocation();
+
+    return (
+        <div className={styles.stepper}>
+            <Link to='/user/details' className={`${styles.step} ${pathname.startsWith('/user/details') ? styles.active : ''}`}>User Details</Link>
+            <Link to='/user/rentals' className={`${styles.step} ${pathname.startsWith('/user/rentals') ? styles.active : ''}`}>Your History</Link>
+            <Link to='/user/stats' className={`${styles.step} ${pathname.startsWith('/user/stats') ? styles.active : ''}`}>Statistics</Link>
+            <Link to='/user/delete-account' className={`${styles.step} ${pathname.startsWith('/user/delete-account') ? styles.active : ''}`}>Delete Account</Link>
+        </div>
+    )
+}
