41a56e9b9392c7fa96893245659f30842ccc348e
Paweł Jarecki <paweljarecki10@gmail.com>
2025-02-03 21:48:32 +0100

    Deleting unnecessary files

--- library-manager-front/src/routes/admin/books/edit/$id.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/books/edit/$id')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/books/edit/$id"!</div>
-}
--- library-manager-front/src/routes/admin/rentals/$id.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/rentals/$id')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/rentals/$id"!</div>
-}
