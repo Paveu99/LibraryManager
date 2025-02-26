850dbca220c4a00a625c841c381e6a291bfc0d14
Paweł Jarecki <paweljarecki10@gmail.com>
2025-02-26 19:22:37 +0100

    Importing vitest packages

--- library-manager-front/vite.config.ts
+++ library-manager-front/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
@@ -5,6 +7,11 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts'
+  },
   server: {
     port: 3000
   }
